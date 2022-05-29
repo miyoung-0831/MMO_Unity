c798141016b15d5a10ae9cfa83058a068e92fb2f
miyoung <miyoung0831@gmail.com>
2022-05-29 13:03:27 +0900

    MMO 유니티 기초

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers Instance; // 유일성이 보장된다
+    static Managers GetInstance() { return Instance; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
